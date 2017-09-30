d380020775dd7bbd53adff8a02f56ecd451cff90
Prannoy Chandra <prannoys21@gmail.com>
2017-09-30 15:39:57 -0700

    removed team

--- TolcProject/src/main/java/com/jwt/controller/EmployeeController.java
+++ TolcProject/src/main/java/com/jwt/controller/EmployeeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import com.jwt.model.Employee;
 import com.jwt.service.EmployeeService;
-//author: Prannoy and Team
+//author: Prannoy and
 @Controller
 public class EmployeeController {
 
